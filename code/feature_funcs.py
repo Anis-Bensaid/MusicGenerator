899b4bf43d0694fda9109d4798de453801b2f546
Desiree Waugh <dwaugh@mit.edu>
2020-05-09 09:41:42 -0500

    Updated feature functions and LSTM

--- code/feature_funcs.py
+++ code/feature_funcs.py
@@ -4,6 +4,7 @@ import pretty_midi
 import music21
 import re
 from random import sample
+import tensorflow as tf
 from keras.utils import to_categorical
 
 # Create vocab dictionaries
@@ -60,5 +61,57 @@ def create_training(char_nums, str_length, vocab_size):
     y = to_categorical(y_data, num_classes=vocab_size)
     
     return x, y
+
+
+def encoder2(text_list, dictionary):
+    '''Convert text data into a numeric list.'''
+    character_nums = np.zeros((len(text_list), len(text_list[0])))
+
+    for row, text in enumerate(text_list):
+        for col, char in enumerate(text):
+            character_nums[row, col] = dictionary[char]
+
+    return character_nums
+
+
+def decoder2(pred_matrix, dictionary):
+    '''Convert numeric list a text string, based on softmax probabilities
+    (rather than argmax).'''
+    my_session = tf.Session()
+    
+    text_list = []
+    
+    samples = tf.random.categorical(pred_matrix, num_samples=1)
+    samples = tf.squeeze(samples, axis=-1)
+    vals = sess.run(samples)
+    
+    text_pred = "".join([dictionary[i] for i in vals])
+        
+    return text_pred
+
+
+def create_training2(song_text_nums, str_length, vocab_size):
+    '''Create training dataset with x and y values from your numeric list.
+    This time, don't separate different songs.'''
+
+    if str_length+1 > len(song_text_nums[0]):
+        return "Your string length is too long for the data."
+
+    # The x_values begin at the start of each song text and are str_length characters long
+    # the y_values are one character after the end of the x_values
+    x_data = song_text_nums[0][0:str_length]
+    y_data = [song_text_nums[0][str_length]]
+    
+    for ind, song in enumerate(song_text_nums[1:], start=1):
+        x_data = np.vstack((x_data, np.array(song[0:str_length])))
+        y_data.append(song[str_length])
+
+    # Convert x and y data to tensors
+    x = to_categorical(x_data, num_classes=vocab_size)
+    y = to_categorical(y_data, num_classes=vocab_size)
+    
+    return x, y
+        
+    
 
 
